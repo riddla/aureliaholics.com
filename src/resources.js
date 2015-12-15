1a216f5852f7341b12898387a295d2a3239bd9e7
Volker Rose <vrose@uandi.com>
2015-12-15 10:54:23 +0100

    Resources content now fetched from github:riddla/awesome-aurelia

--- src/resources.js
+++ src/resources.js
@@ -1,13 +1,21 @@
 import {inject} from 'aurelia-framework';
-import 'fetch';
+import {HttpClient} from 'aurelia-http-client';
+import {parse} from 'marked';
 
+@inject(HttpClient)
 export class Resources {
   heading = 'Learning Resources';
 
   constructor(http) {
+    this.marked = parse;
+    this.http = http;
   }
 
-  activate() {
-
+  activate(params, routeConfig, navigationInstruction) {
+    return this.http.get('https://cdn.rawgit.com/riddla/awesome-aurelia/master/README.md')
+      .then(response => {
+        this.content = response.response;
+        this.formattedContent = this.marked(this.content);
+      });
   }
 }
